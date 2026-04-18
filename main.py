5616f29470021ccb1e70484ef9847c58e529e892
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:23:17 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def tekshir():
-    son = int(input("Son kiriting: "))
-    if son >= 10 and son < 100:
-        print("Son 2 xonali")
-    else:
-        print("Son 2 xonali emas")
+def juft_toq_sonlar(N):
+    juft_sonlar = []
+    toq_sonlar = []
+    for i in range(1, N+1):
+        if i % 2 == 0:
+            juft_sonlar.append(i)
+        else:
+            toq_sonlar.append(i)
+    print("Juft sonlar: ", juft_sonlar)
+    print("Toq sonlar: ", toq_sonlar)
 
-tekshir()
+N = int(input("1 dan N gacha sonlarni kiriting: "))
+juft_toq_sonlar(N)
